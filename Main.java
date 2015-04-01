d320d0a1b941c71f7fbfd715aeb6b4a15c2210c5
futurnaingenieur <futurnaingenieur@gmail.com>
2015-04-01 02:28:00 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+	
+	
+	/*
+	 * This is the main program launching a SelectLevel frame
+	 * which will ask the user what kind of difficulty
+	 * he wants.
+	 */
+		
+	public static void main(String[] args) {
+		
+		SelectLevel frame = new SelectLevel("Main Menu");
+
+	}
+
+}
